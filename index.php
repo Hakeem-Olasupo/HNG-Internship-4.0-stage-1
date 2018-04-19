6c1c88ffcb6fb4f4012fec758e8f8b864cd435e5
sekinat <sekinat95@gmail.com>
2018-04-19 11:40:46 +0200

    adding all files

---
+++ index.php
@@ -0,0 +1,23 @@
+ <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>HNG 4.0 Internship - stage-1 task </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+   
+</head>
+<body>
+    <div id="container" >
+        <center>
+            <h3>HELLO, I'm Kunmi<br>
+                its: <span><?php echo  date("h:ia");  ?></span><br>
+                glad to be on HNG 4.0
+          
+        </center>
+    </div>
+</body>
+</html>
